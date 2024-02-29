9e4ef6c65d1460b4c1260d2ef9e68119df49b873
KushTurner <kushturner9@gmail.com>
2024-02-29 12:29:09 +0000

    Added kyrie

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -10,8 +10,10 @@ public class QueryProcessor {
       return "William Shakespeare (26 April 1564 - 23 April 1616) was an "
           + "English poet, playwright, and actor, widely regarded as the greatest "
           + "writer in the English language and the world's pre-eminent dramatist.";
+    } else if (query.toLowerCase().contains("kyrie")) {
+      return "Kyrie Irving (born March 23, 1992) is an American professional "
+          + "basketball player for the Dallas Mavericks of the National Basketball Association (NBA).";
     }
-
     return "";
   }
 }
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -20,4 +20,9 @@ public class QueryProcessorTest {
     assertThat(queryProcessor.process("Shakespeare"), containsString("playwright"));
   }
 
+  @Test
+  public void knowsAboutKyrie() throws Exception {
+    assertThat(queryProcessor.process("Kyrie"), containsString("basketball"));
+  }
+
 }
